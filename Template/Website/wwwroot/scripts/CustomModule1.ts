6170b4b58355bcdaf7ce4cd03eb35366d4f8b5b4
Paymon <paymon@geeks.ltd.uk>
2018-01-21 11:56:04 +0000

    no message

---
+++ Template/Website/wwwroot/scripts/CustomModule1.ts
@@ -0,0 +1,11 @@
+﻿
+// Note: This module should be loaded via requirejs.
+// On the page that this is used (or the layout cshtml file) use:
+//    loadModule('/scripts/CustomModule1', m => m.default.Run());
+
+export default class CustomModule1 {
+
+    public static Run(): void {
+        // TODO: ... 
+    }
+}
