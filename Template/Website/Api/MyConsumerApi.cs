350f24ac56ce2b4bbd07917a74ba45521193221d
Paymon <paymon@geeks.ltd.uk>
2018-01-31 08:48:40 +0000

    no message

---
+++ Template/Website/Api/MyConsumerApi.cs
@@ -0,0 +1,45 @@
+﻿namespace MY.MICROSERVICE.NAMEService
+{
+    using System;
+    using Domain;
+    using System.Linq;
+    using System.ComponentModel;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+    using Olive.Entities;
+    using Olive.Mvc;
+    using Microsoft.AspNetCore.Http;
+    using Olive;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Controllers;
+
+    /* NOTE ===================================
+    *  See this before creating your API:
+    *  https://github.com/Geeksltd/Olive/blob/master/Integration/Olive.Microservices/Integration.md
+    *  ======================================== */
+
+    [Route("api")]
+    public class MyConsumerApi : BaseController
+    {
+        public class MyResultType
+        {
+            public string Property1;
+            public string[] Property2;
+        }
+
+        [HttpGet, Route("my-function")]
+        [Returns(typeof(MyResultType))]
+        // [AuthorizeTrustedService]  or  [AuthorizeService("ServiceName")]  or  [Authorize(Roles = "...")]
+        public async Task<IActionResult> MyFunction(string param1)
+        {
+            var result = new MyResultType
+            {
+                Property1 = "...",
+                Property2 = new[] { "...", "..." },
+            };
+
+            return Json(result);
+        }
+    }
+}
