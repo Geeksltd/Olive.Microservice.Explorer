930ba1bdc9bf54b5d1994891f63a4dd0660db897
Farzad <pharzath@gmail.com>
2018-04-11 19:24:03 +0430

    Init

--- MacroserviceGridItem.cs
+++ MacroserviceGridItem.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Documents;
-using System.Windows.Forms;
 using MacroserviceExplorer.Annotations;
 
 namespace MacroserviceExplorer
