2e597794b9f3c4289578c59784910c7015bbd1fc
Paymon <paymon@geeks.ltd.uk>
2018-07-30 17:32:06 +0100

    no message

--- Template/Website/Controllers/BaseController.cs
+++ Template/Website/Controllers/BaseController.cs
@@ -1,15 +1,16 @@
 ﻿using Domain;
+using Olive;
 using Olive.Mvc;
 
 namespace Controllers
 {
     public class BaseController : Olive.Mvc.Controller
     {
-		public BaseController()
+        public BaseController()
         {
             ApiClient.FallBack.Handle(arg => Notify(arg.FriendlyMessage, false));
         }
-		
+
         // Here you can add helper methods to all your controllers.
     }
 }
