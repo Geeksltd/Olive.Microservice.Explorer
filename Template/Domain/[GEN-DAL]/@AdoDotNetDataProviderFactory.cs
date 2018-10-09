a92985e356776ced94ae6c93599fd0506f284298
MRP <ipazooki@gmail.com>
2018-10-09 17:52:52 +0330

    update template

--- Template/Domain/[GEN-DAL]/@AdoDotNetDataProviderFactory.cs
+++ Template/Domain/[GEN-DAL]/@AdoDotNetDataProviderFactory.cs
@@ -14,25 +14,27 @@
     [EscapeGCop("Auto generated code.")]
     public class AdoDotNetDataProviderFactory : IDataProviderFactory
     {
+        ICache Cache;
         string ConnectionStringKey;
-        public string ConnectionString {get; private set;}
-        
+        public string ConnectionString { get; private set; }
+
         /// <summary>Initializes a new instance of AdoDotNetDataProviderFactory.</summary>
         public AdoDotNetDataProviderFactory(DatabaseConfig.Provider provider)
         {
             ConnectionString = provider.ConnectionString.Or(DataAccess.GetCurrentConnectionString());
             ConnectionStringKey = provider.ConnectionStringKey;
+            Cache = Context.Current.GetService<ICache>();
         }
-        
+
         public IDataAccess GetAccess() => new DataAccess<SqlConnection>();
-        
+
         /// <summary>Gets a data provider instance for the specified entity type.</summary>
         public virtual IDataProvider GetProvider(Type type)
         {
             IDataProvider result = null;
-            
+
             if (type.IsInterface) result = new InterfaceDataProvider(type);
-            
+
             if (result == null)
             {
                 throw new NotSupportedException(type + " is not a data-supported type.");
@@ -45,10 +47,10 @@
             {
                 result.ConnectionStringKey = this.ConnectionStringKey;
             }
-            
+
             return result;
         }
-        
+
         /// <summary>Determines whether this data provider factory handles interface data queries.</summary>
         public virtual bool SupportsPolymorphism() => true;
     }
