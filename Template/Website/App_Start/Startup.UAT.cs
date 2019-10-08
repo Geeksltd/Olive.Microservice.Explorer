0e421066abe7d37b82de1377c9ca85d30c8d7a62
MRP <ipazooki@gmail.com>
2019-10-08 16:39:04 +0330

    update nuget packages

---
+++ Template/Website/App_Start/Startup.UAT.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Olive;
+
+namespace Website
+{
+    public class StartupUAT : Startup
+    {
+        public StartupUAT(IHostingEnvironment env, IConfiguration config, ILoggerFactory factory) : base(env, config, factory)
+        {
+        }
+
+        protected override void SetUpIdentity(IHostingEnvironment env, IConfiguration config)
+        {
+            config.LoadAwsIdentity();
+        }
+    }
+}
